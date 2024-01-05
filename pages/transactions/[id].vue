fbc3418e2091f426ba0fe404e3a7f3477ef4be52
Diogo Ferreira <DiogoFerreira_02@protonmail.com>
2024-01-05 16:37:45 +0100

    added transaction info page

--- pages/transactions/[id].vue
+++ pages/transactions/[id].vue
@@ -1,11 +1,33 @@
 <template>
     <div>
-        <h2>Transaction id: {{ id }} </h2>
+        <div>Amount: {{ transaction.amount }}</div>
+        <div>Date: {{ transaction.date }}</div>
+        <div>Recipient: {{ transaction.recipient }}</div>
+        <div>Notes: {{ transaction.notes }}</div>
     </div>
 </template>
 
 <script setup>
+import { useRuntimeConfig } from '#app'
 
+const runtimeConfig = useRuntimeConfig()
 const { id } = useRoute().params
 
+const transaction = ref({})
+
+const getTransactions = async () => {
+    try {
+        let response = await $fetch(runtimeConfig.public.BACKEND_API_BASE_PATH + '/transactions/' + id, {
+            method: 'GET',
+        })
+
+        //turn date into a readable format
+        response.date = response.date.split('T')[0];
+
+        transaction.value = response;
+    } catch (error) {
+        console.error('ERROR:', error)
+    }
+}
+getTransactions();
 </script>
